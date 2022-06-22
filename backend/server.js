716488aeba10e277569e18d1057451c5cbc43a35
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-06-22 16:57:50 -0700

    server running, .env set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log('hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+app.listen(port, ()=> console.log(`Server running on port ${port}`))
