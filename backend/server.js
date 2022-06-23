2cfbb989ccc4b570b3b91230eeb90689737a6635
Sam <bezzantsam@gmail.com>
2022-06-23 02:23:56 -0700

    working controllers and CRUD routes

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,8 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+
 app.listen(port, ()=> console.log(`Server running on port ${port}`))
