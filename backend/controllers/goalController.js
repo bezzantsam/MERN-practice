84dc7c376acbd94b703fcf93388c224ed5c01c60
Sam <bezzantsam@gmail.com>
2022-06-24 16:28:20 -0700

    controllers crud up to post no delete yet

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -6,7 +6,9 @@ const Goal = require('../models/goalModel')
 //@route  GET/api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 //@ desc Set goal
@@ -18,15 +20,30 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('No goal text provided')
     }
     
+    const goal = await Goal.create({
+        text: req.body.text
+    })
     
-    res.status(200).json({message: 'Set goal'})
+    res.status(200).json(goal)
 })
 
 //@ desc Update goal
 //@route  Put /api/goals/:id
 //@access Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message:`Update goal ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(404)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 //@ desc Delete goal
